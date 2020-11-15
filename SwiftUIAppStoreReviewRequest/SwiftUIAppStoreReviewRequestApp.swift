1cf8f1fae29de6b63225f7bf6b3a64c2bc165f76
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-15 19:28:17 +0200

    Initial Commit

---
+++ SwiftUIAppStoreReviewRequest/SwiftUIAppStoreReviewRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppStoreReviewRequestApp.swift
+//  SwiftUIAppStoreReviewRequest
+//
+//  Created by Ramill Ibragimov on 15.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppStoreReviewRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
